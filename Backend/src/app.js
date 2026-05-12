bc41eadf78ce802bd49bb196c82f97d44302e3ab
devshanuuu <shanuuu0207@gmail.com>
2026-05-12 16:54:33 +0530

    CORS middleware added

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,11 +1,16 @@
 const express = require('express');
 const authRouter = require('./routes/auth.routes');
 const cookieParser = require('cookie-parser');
+const cors = require('cors');
 
 const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
+app.use(cors({
+    origin: 'http://localhost:5173', // This is the URL of our frontend application. 
+    credentials: true
+}))
 
 /*Using all the routes here*/
 app.use('/api/auth', authRouter)
